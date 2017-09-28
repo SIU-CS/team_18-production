4440a6401f270be98745ccc3dba06e7275d7c95a
Dakota Mitchem <dakota.mitchem@siu.edu>
2017-09-28 17:54:50 -0500

    Added initial Android studio files

---
+++ FinancialAdvisorApp/app/src/main/java/team_18_production/financialadvisorapp/MainActivity.java
@@ -0,0 +1,13 @@
+package team_18_production.financialadvisorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
