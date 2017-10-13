695a02af77c41e642d0c791eaf80a20eefc7c6cd
Unknown <iantodd96@siu.edu>
2017-10-12 19:09:10 -0500

    Added all screens, coded budget summary button.

---
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/BudgetSummary.java
@@ -0,0 +1,13 @@
+package team_18.financialadvisor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BudgetSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_summary);
+    }
+}
--- FinancialAdvisor/app/src/main/java/team_18/financialadvisor/MainActivity.java
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/MainActivity.java
@@ -1,7 +1,10 @@
 package team_18.financialadvisor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button budgetSummary = (Button)findViewById(R.id.buttonBudgetSummary);
+
+        //Setting button behavior
+        budgetSummary.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent myIntent = new Intent(MainActivity.this, BudgetSummary.class);
+                startActivity(myIntent);
+            }
+        });
+
+        }
+
     }
-}
+
