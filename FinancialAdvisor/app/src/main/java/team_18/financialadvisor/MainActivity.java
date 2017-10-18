9547821c18a47fa2bd174ace6014ca06a8c006b6
Unknown <iantodd96@siu.edu>
2017-10-18 17:48:51 -0500

    Minor changes just to push and pull this branch

--- FinancialAdvisor/app/src/main/java/team_18/financialadvisor/MainActivity.java
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/MainActivity.java
@@ -1,17 +1,11 @@
 package team_18.financialadvisor;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import android.content.Context;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+        OnTouchSwipeListener onSwipeTouchListener = new OnTouchSwipeListener(MainActivity.this) {
+            @Override
+            public void onSwipeLeft() {
+                //your actions
+            }
+        };
 
         }
 
