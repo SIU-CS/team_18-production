ecea2a1b4658cfcbd51fd1eb1ba0732806004b21
Dakota Mitchem <dakota.mitchem@siu.edu>
2017-10-18 18:04:41 -0500

    Updated PseudoDatabase

---
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/PseudoDatabase.java
@@ -0,0 +1,12 @@
+/**
+*Class is used to test functionality of app prior to developing the database by acting as a surrogate for the database
+*/
+public class PseudoDatabase{
+		
+	
+	
+	
+	
+	
+	
+}
