a7650e3628f0d1c5d3bfc1da4af845d4958f2c71
Unknown <iantodd96@siu.edu>
2017-10-18 17:20:45 -0500

    Updated theme, made a few changes to some names

---
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/OnTouchSwipeListener.java
@@ -0,0 +1,20 @@
+package team_18.financialadvisor;
+
+/**
+ * Created by SIU853108493 on 10/18/2017.
+ */
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+
+public class OnTouchSwipeListener {
+    private final GestureDetector gestureDetector;
+    private Context context;
+
+
+}
