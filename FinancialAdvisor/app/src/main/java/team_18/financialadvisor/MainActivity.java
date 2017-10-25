ee1e8f39fa3954fd2f9ff482ad9d345a34c63d89
Unknown <avtar.singh@siu.edu>
2017-10-24 21:04:23 -0500

    Added working list view for upcoming bills

--- FinancialAdvisor/app/src/main/java/team_18/financialadvisor/MainActivity.java
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/MainActivity.java
@@ -6,9 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lst;
+    String[] months={"11/10/17: Car Payment $150.00","11/15/17: Phone Bill $80.00","11/29/17: Internet Bill $30.00"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent myIntent = new Intent(MainActivity.this, GraphView.class);
                 startActivity(myIntent);
+            }
+        });
+
+        lst= (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> arrayadapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,months);
+        lst.setAdapter(arrayadapter);
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv= (TextView) view;
+                Toast.makeText(MainActivity.this,tv.getText()+"  "+position,Toast.LENGTH_LONG).show();
             }
         });
 
