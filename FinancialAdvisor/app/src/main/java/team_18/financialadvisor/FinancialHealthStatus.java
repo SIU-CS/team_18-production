590430a65c221d0fb627bb06a75867d2b0046933
Dakota Mitchem <dakota.mitchem@siu.edu>
2017-10-30 22:09:33 -0500

    Added code to financial Health

--- FinancialAdvisor/app/src/main/java/team_18/financialadvisor/FinancialHealthStatus.java
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/FinancialHealthStatus.java
@@ -1,6 +1,7 @@
 package team_18.financialadvisor;
 
 
+
 /**
  * Created by Dakota on 10/30/2017.
  */
@@ -31,8 +32,59 @@ public class FinancialHealthStatus {
         setWeeksDelinquent(database);
         setWeeksUsed(database);
     }
+
+    public int currentBalancePoints(){
+        int points = 0;
+         if(getCurrentBalance()<=50)
+             points=5;
+         else if (getCurrentBalance()<=100)
+             points=10;
+        else if (getCurrentBalance()<=150)
+            points=15;
+        else if(getCurrentBalance()<=200)
+            points=20;
+        else if (getCurrentBalance()>200)
+            points=25;
+        return points;
+    }
+
+    public int expensesPoints(){
+        int points = 0;
+        if(getExpensesRemaining()<=50)
+            points=3;
+        else if (getExpensesRemaining()<=100)
+            points=2;
+        else if (getExpensesRemaining()<=150)
+            points=1;
+        return points;
+    }
+    public int savingsPoints(){
+        int points = 0;
+        if(getTotalSavings()<=250)
+            points=1;
+        else if (getTotalSavings()<=500)
+            points=2;
+        else if (getTotalSavings()<=1500)
+            points=3;
+        else if(getTotalSavings()<=5000)
+            points=4;
+        else if (getTotalSavings()>5000)
+            points=5;
+        return points;
+
+    }
+
+    public int delinquentPoints(){
+
+    }
+
     public String generateStatus(){
-        
+        int points = (currentBalancePoints()+expensesPoints()+ savingsPoints()+)
+        String status=null;
+        if (points)
+
+
+        return status;
     }
 
 
