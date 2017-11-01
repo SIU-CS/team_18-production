69c9881d4fac26119f926f720b031202d41a35a6
Unknown <avtar.singh@siu.edu>
2017-11-01 11:52:48 -0500

    Added working Main menu and Clear button to AddIncome activity

--- FinancialAdvisor/app/src/main/java/team_18/financialadvisor/AddIncome.java
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/AddIncome.java
@@ -5,8 +5,11 @@ package team_18.financialadvisor;
  */
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class AddIncome extends AppCompatActivity {
@@ -15,8 +18,25 @@ public class AddIncome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_income);
+        Button buttonGVGoToMM = (Button)findViewById(R.id.MMButtonGVGoToMM);
+        final Button reset = (Button)findViewById(R.id.button_clear);
 
+        buttonGVGoToMM.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent myIntent = new Intent(AddIncome.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
 
+        reset.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (v==reset) {
+                    startActivity(new Intent(AddIncome.this, AddIncome.class));
+                }
+            }
+        });
 
     }
 }
