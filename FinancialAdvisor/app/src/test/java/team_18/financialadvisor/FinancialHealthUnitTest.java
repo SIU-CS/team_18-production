9e16cd4f33b4598382bc7842591c82c0a92d9b09
Dakota Mitchem <dakota.mitchem@siu.edu>
2017-12-06 19:11:16 -0600

    Changed test

--- FinancialAdvisor/app/src/test/java/team_18/financialadvisor/FinancialHealthUnitTest.java
+++ FinancialAdvisor/app/src/test/java/team_18/financialadvisor/FinancialHealthUnitTest.java
@@ -1,10 +1,14 @@
 /*package team_18.financialadvisor;
 
+import android.content.Context;
+import android.database.Cursor;
+
 import org.junit.Test;
 
 import team_18.financialadvisor.data.DBHelper;
 import team_18.financialadvisor.data.DatabaseManager;
 import team_18.financialadvisor.data.model.BudgetData;
+import team_18.financialadvisor.data.repo.BudgetDataRepo;
 
 import static org.junit.Assert.*;
 
@@ -17,12 +21,31 @@ import static org.junit.Assert.*;
 public class FinancialHealthUnitTest {
     @Test
     public void FinancialHealth() throws Exception {
+        Context context;
+        DBHelper dbHandler;
+        BudgetDataRepo.setBudget();
+        getDBdata();
+        dbHandler = new DBHelper();
+        DatabaseManager.initializeInstance(dbHandler);
         final FinancialHealthStatus health =new FinancialHealthStatus();
-        BudgetData newData = App.getDBdata();
-        DBHelper helper=new DBHelper();
-        DatabaseManager.initializeInstance(helper);
+
         String status= health.generateStatus();
         assertEquals("Poor", status);
+    }
+    public static BudgetData getDBdata() {
+
+        Cursor allData = BudgetDataRepo.getAllData();
+        // BudgetData allBudgetData = new BudgetData();
+
+        if(allData != null && allData.moveToFirst()) {
+
+            BudgetData setBudgetData = new BudgetData(allData.getInt(0),allData.getDouble(1),
+                    allData.getDouble(2), allData.getDouble(3),allData.getInt(4),
+                    allData.getInt(5), allData.getInt(6), allData.getInt(7));
+            return setBudgetData;
+        }
+
+        return null;
     }
 }
 */
