66be00f4a88948423d00672147d37bf0a7a26656
Unknown <amen89@siu.edu>
2017-12-06 20:10:32 -0600

    Made changes to GraphView.java, activity_graph_view and the build gradle files

--- FinancialAdvisor/app/src/main/java/team_18/financialadvisor/GraphView.java
+++ FinancialAdvisor/app/src/main/java/team_18/financialadvisor/GraphView.java
@@ -1,21 +1,35 @@
 package team_18.financialadvisor;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class GraphView extends AppCompatActivity {
+
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_view);
 
-        Button buttonGVGoToBS = (Button)findViewById(R.id.buttonGVGoToBS);
-        Button buttonGVGoToMM = (Button)findViewById(R.id.MMButtonGVGoToMM);
-        Button buttonGVGoToCV = (Button)findViewById(R.id.buttonGVGoToCV);
+        Button buttonGVGoToBS = (Button) findViewById(R.id.buttonGVGoToBS);
+        Button buttonGVGoToMM = (Button) findViewById(R.id.MMButtonGVGoToMM);
+        Button buttonGVGoToCV = (Button) findViewById(R.id.buttonGVGoToCV);
+
 
         //Setting button behavior
         buttonGVGoToBS.setOnClickListener(new View.OnClickListener(){
@@ -39,5 +53,28 @@ public class GraphView extends AppCompatActivity {
             }
         });
 
+        barChart = (BarChart) findViewById(R.id.barchart);
+
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(true);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+
+        barEntries.add(new BarEntry(1, 40f));
+        barEntries.add(new BarEntry(2, 48f));
+        barEntries.add(new BarEntry(3, 37f));
+        barEntries.add(new BarEntry(4, 50f));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Data Set1");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData theData = new BarData(barDataSet);
+        theData.setBarWidth(0.9f);
+
+        barChart.setData(theData);
     }
 }
+
